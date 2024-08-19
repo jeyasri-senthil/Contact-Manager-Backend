bd7b4847c890d47caa9073a57343fc0ad04a6fc3
JEYASRI S <108861190+jeyasri-senthil@users.noreply.github.com>
2024-08-19 17:14:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// server.js
-
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -11,25 +9,10 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-
 app.use('/api/contacts',contacts);
 
 const conn = require('./services/db');
 conn.dbConnection();
-
-// // MongoDB Connection
-// mongoose.connect('mongodb://localhost:27017/contactmanager', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// mongoose.connection.once('open', () => {
-//   console.log('Connected to MongoDB');
-// });
-
-// // Routes
-// const contactRoutes = require('./routes/contacts');
-// app.use('/api/contacts', contactRoutes);
 
 app.listen(4000, () => {
   console.log(`Server is running on port 4000`);
